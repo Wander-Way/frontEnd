d771188718b0c89a9754cc0bfbdb949a2e517297
류진호 <dbwlsgh57@naver.com>
2023-11-17 16:57:54 +0900

    [Feat] : 게시판 정보 출력까지 구현

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -12,10 +12,10 @@
         <router-link :to="{ name: 'anotherRoute' }">여행루트</router-link>
       </li>
       <li>
-        <router-link :to="{}">핫플정보</router-link>
+        <router-link :to="{ name: 'hotPlace' }">핫플정보</router-link>
       </li>
       <li>
-        <router-link :to="{}">로그인/내정보</router-link>
+        <router-link :to="{ name: 'myPage' }">로그인/내정보</router-link>
       </li>
     </ul>
   </div>
