65b3529206b6fc3695e550050262de671c64be18
류진호 <dbwlsgh57@naver.com>
2023-11-20 11:28:01 +0900

    [Feat] 찜 목록, 내여행목록 기능 연결 완료

--- src/views/MypageView/MyPageMyLikeList.vue
+++ src/views/MypageView/MyPageMyLikeList.vue
@@ -1,9 +1,27 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { useMyPageStore } from "../../stores/mypage.js";
+import { RouterLink, useRouter } from "vue-router";
+import { storeToRefs } from "pinia";
+
+const myPageStore = useMyPageStore();
+const router = useRouter();
+const myWishListLocal = ref({}); //여기에 저장되구요
+
+const requestMypage = async () => {
+  await myPageStore.requestMyLikeList();
+  const { myLikeListInfo } = storeToRefs(myPageStore);
+  myWishListLocal.value = myLikeListInfo.value;
+  console.log(myWishListLocal.value);
+};
+
+onMounted(requestMypage);
+</script>
 
 <template>
   <div>
     <h1>찜 목록</h1>
-    </div>
+  </div>
 </template>
 
 <style scoped></style>
